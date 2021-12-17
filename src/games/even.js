67234326ee9b4b311d434a33dbf97e5c84ccb750
Matheria <sensenevercame@gmail.com>
2021-12-18 03:07:54 +0500

    add brain-even game

---
+++ src/games/even.js
@@ -0,0 +1,31 @@
+import readlineSync from 'readline-sync';
+import getRandomNumber from '../utils.js';
+
+const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
+
+const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
+const gameCount = 3;
+
+const brainEvenGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log(gameRules);
+
+  for (let count = 1; count <= gameCount; count += 1) {
+    const num = getRandomNumber();
+    const question = `Question: ${num}`;
+    console.log(question);
+    const userAnswer = readlineSync.question('Answer: ');
+    if (userAnswer === isEven(num)) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${isEven(num)}'.`);
+      return console.log(`Let's try again, ${name}`);
+    }
+  }
+
+  return console.log(`Congratulations, ${name}`);
+};
+
+export default brainEvenGame;
